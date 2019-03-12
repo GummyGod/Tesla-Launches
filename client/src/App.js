f36740e31b3bbf026e9fa498de900ba6ab58e0d6
GummyGod <gummy.pwnn@yahoo.com>
2019-03-12 23:33:29 +0200

    launches && intialize apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.css';
+import logo from './logo.jpg';
+
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Spacex </h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{width:300, display:'block', margin:'auto'}} />
+          <Launches />
+        </div>
+      </ApolloProvider>
     );
   }
 }
